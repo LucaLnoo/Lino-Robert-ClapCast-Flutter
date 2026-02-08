ff73139c51c5a7ed87724bd6dcb4ea868e05005d
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-09 00:26:02 +0100

    Add PersonOverview and PersonFilmography in person model

---
+++ ClapCastFlutter/lib/app/model/person.dart
@@ -0,0 +1,80 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'movie.dart';
+
+part 'person.g.dart';
+
+// --- Person LIST -- //
+@JsonSerializable(explicitToJson: true)
+class People {
+  @JsonKey(name: "results")
+  final List<PersonOverview> personList;
+
+  People(this.personList);
+
+  factory People.fromJson(Map<String, dynamic> json) => _$PeopleFromJson(json);
+  Map<String, dynamic> toJson() => _$PeopleToJson(this);
+}
+
+// --- PERSON OVERVIEW -- //
+@JsonSerializable(explicitToJson: true)
+class PersonOverview{
+  @JsonKey(name:"id")
+  final int id;
+
+  //0	Not specified
+  //1	Female
+  //2	Male
+  //3	Non-binary
+  @JsonKey(name: "gender") // Utile si nom diff
+  final int? gender;
+
+  @JsonKey(name: "name")
+  final String? name;
+
+  @JsonKey(name: "adult", defaultValue: true)
+  final bool adult;
+
+  @JsonKey(name: "known_for_department")
+  final String? department;
+
+  @JsonKey(name: "profile_path")
+  final String? profilePathImage;
+
+  @JsonKey(name: "popularity")
+  final double? popularityRate;
+
+  @JsonKey(name: "known_for")
+  final List<MovieDetails> knownForMovies;
+
+  PersonOverview({
+    required this.id,
+    this.gender,
+    this.adult = true,
+    this.name,
+    this.department,
+    this.profilePathImage,
+    this.popularityRate,
+    this.knownForMovies = const [],
+  });
+
+  factory PersonOverview.fromJson(Map<String, dynamic> json) => _$PersonOverviewFromJson(json);
+  Map<String, dynamic> toJson() => _$PersonOverviewToJson(this);
+}
+
+@JsonSerializable()
+class PersonFilmography{
+  @JsonKey(name:"cast", defaultValue: [])
+  final List<MovieOverview> cast;
+
+  @JsonKey(name:"crew", defaultValue: [])
+  final List<MovieOverview> crew;
+
+  PersonFilmography({
+    this.cast = const [],
+    this.crew = const [],
+  });
+
+  factory PersonFilmography.fromJson(Map<String, dynamic> json) => _$PersonFilmographyFromJson(json);
+  Map<String, dynamic> toJson() => _$PersonFilmographyToJson(this);
+}
