0a42f50cd237a16d49e275f4eacd9dbbe58c4fb3
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-08 21:53:11 +0100

    Add function to get a list of movie overview

--- ClapCastFlutter/lib/app/repository/movie_repository.dart
+++ ClapCastFlutter/lib/app/repository/movie_repository.dart
@@ -6,7 +6,27 @@ final movieRepository = _MovieRepository();
 
 class _MovieRepository {
 
-  Future<Movies> getMovies() {
-    return apiCinema.getMovies();
+  Future<List<MovieOverview>> getMoviesOverview(String? title) async {
+    try{
+      List<MovieOverview> movies = [];
+      const int pageRange = 20;
+
+      if(title == null){
+        for(int page=1; page<pageRange; page++){
+          final fetchMovies = await apiCinema.movieService.getMoviesOverviewPage(page);
+          movies.addAll(fetchMovies.movieList);
+        }
+      }else{
+        final fetchMovies = await apiCinema.movieService.searchMovies(title);
+        movies.addAll(fetchMovies.movieList);
+      }
+
+      var filterMovies = movies.where((movie) => movie.posterPathImage?.isNotEmpty ?? false).toList();
+      return filterMovies;
+    }catch(error){
+      throw Exception( error);
+    }
   }
+
+
 }
