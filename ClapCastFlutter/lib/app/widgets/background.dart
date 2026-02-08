317f0292867a0b4d1dc8bec76ba73201458d7fab
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-08 15:12:47 +0100

    AppLoginBackground widget (gradient background)

--- ClapCastFlutter/lib/app/widgets/background.dart
+++ ClapCastFlutter/lib/app/widgets/background.dart
@@ -19,3 +19,34 @@ class AppMenuBackground extends StatelessWidget{
     );
   }
 }
+
+class AppLoginBackground extends StatelessWidget{
+  final Widget child;
+
+  const AppLoginBackground({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+
+          colors: [
+            AppColor.btnPrimary,
+            AppColor.appBackground,
+            AppColor.appBackground,
+          ],
+          stops: [0.0, 0.27, 1.0],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
