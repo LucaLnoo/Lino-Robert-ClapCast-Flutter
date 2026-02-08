f1acc3018b20ca1b2448359ee29a3f8069d3ed06
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-08 16:35:16 +0100

    Change structure to differentiate API configuration and service

--- ClapCastFlutter/lib/app/api/api.dart
+++ ClapCastFlutter/lib/app/api/api.dart
@@ -1,34 +1,41 @@
 
-import 'package:clapcastflutter/app/model/movie.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
 
-part 'api.g.dart';
+import 'cinema_service.dart';
 
 final apiCinema = Api();
 
-@RestApi()
-abstract class Api{
-  factory Api(){
-    final dio = Dio(
+class Api{
+  late final Dio _dio;
+
+  late final MovieService movieService;
+
+  // Singleton
+  static final Api _instance = Api._internal();
+
+  factory Api() {
+    return _instance;
+  }
+
+  Api._internal() {
+    _dio = Dio(
       BaseOptions(
         baseUrl: "https://api.themoviedb.org/3/",
         contentType: "application/json",
-        headers: <String, dynamic> {
+        headers: <String, dynamic>{
           "accept": "application/json",
           "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGE5YWVhNTViZDQ2Y2M0OTYwY2MzMTA3ZWEyNDhiYyIsIm5iZiI6MTc1NzU4MDc1OS4yMjgsInN1YiI6IjY4YzI4ZGQ3NGFjZmUxNGU4ZTFiZGNkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dz9Vls_URy6UgBiveLGL-URdZmS4iL_0VSIw7Uo-Wzs"
-        }
-      )
+        },
+      ),
     );
 
-    dio.interceptors.add(LogInterceptor(
-        responseBody: true,
-        requestHeader: true,
-        responseHeader: true
+    _dio.interceptors.add(LogInterceptor(
+      responseBody: true,
+      requestHeader: true,
+      responseHeader: true,
     ));
-  return _Api(dio);
-  }
 
-  @GET("movie/popular")
-  Future<Movies> getMovies();
+    movieService = MovieService(_dio);
+  }
 }
