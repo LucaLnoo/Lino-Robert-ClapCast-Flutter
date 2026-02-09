1639e9257df9e8e1be15b7a32abcb7acf84efaa5
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-09 21:56:37 +0100

    private merge function to get the credit of actors for their detailed page

--- ClapCastFlutter/lib/app/repository/person_repository.dart
+++ ClapCastFlutter/lib/app/repository/person_repository.dart
@@ -5,24 +5,44 @@ final personRepository = _PersonRepository();
 
 class _PersonRepository {
   Future<List<PersonOverview>> getPeopleOverview({String? title}) async {
-    try{
+    try {
       List<PersonOverview> people = [];
       const int pageRange = 20;
 
-      if(title == null){
-        for(int page=1; page<pageRange; page++){
-          final fetchPeople = await apiCinema.actorService.getPeopleOverviewPage(page);
+      if (title == null) {
+        for (int page = 1; page < pageRange; page++) {
+          final fetchPeople = await apiCinema.actorService
+              .getPeopleOverviewPage(page);
           people.addAll(fetchPeople.personList);
         }
-      }else{
-        final fetchPeople = await apiCinema.actorService.searchPeopleOverview(title);
+      } else {
+        final fetchPeople = await apiCinema.actorService.searchPeopleOverview(
+            title);
         people.addAll(fetchPeople.personList);
       }
 
-      var filterPeople = people.where((person) => person.profilePathImage?.isNotEmpty ?? false).toList();
+      var filterPeople = people.where((person) =>
+      person.profilePathImage?.isNotEmpty ?? false).toList();
       return filterPeople;
-    }catch(error){
-      throw Exception( error);
+    } catch (error) {
+      throw Exception(error);
     }
+  }
+
+
+  PersonDetails _mergeDetailsAndCredits(PersonDetails person,
+      PersonFilmography credits) {
+    return PersonDetails(
+      id: person.id,
+      name: person.name,
+      department: person.department,
+      biography: person.biography,
+      birthday: person.birthday,
+      placeOfBirth: person.placeOfBirth,
+      popularityRate: person.popularityRate,
+      profilePathImage: person.profilePathImage,
+
+      knownForMovies: credits,
+    );
   }
 }
