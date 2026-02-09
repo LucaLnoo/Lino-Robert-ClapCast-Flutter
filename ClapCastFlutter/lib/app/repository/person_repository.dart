67142f31fa982eafcadef5b5fb54217bddd3a434
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-09 21:50:32 +0100

    getPeopleOverview method in person_repository to get the starting list of actor

--- ClapCastFlutter/lib/app/repository/person_repository.dart
+++ ClapCastFlutter/lib/app/repository/person_repository.dart
@@ -1,5 +1,28 @@
+import 'package:clapcastflutter/app/api/api.dart';
+import 'package:clapcastflutter/app/model/person.dart';
+
 final personRepository = _PersonRepository();
 
 class _PersonRepository {
+  Future<List<PersonOverview>> getPeopleOverview({String? title}) async {
+    try{
+      List<PersonOverview> people = [];
+      const int pageRange = 20;
 
+      if(title == null){
+        for(int page=1; page<pageRange; page++){
+          final fetchPeople = await apiCinema.actorService.getPeopleOverviewPage(page);
+          people.addAll(fetchPeople.personList);
+        }
+      }else{
+        final fetchPeople = await apiCinema.actorService.searchPeopleOverview(title);
+        people.addAll(fetchPeople.personList);
+      }
+
+      var filterPeople = people.where((person) => person.profilePathImage?.isNotEmpty ?? false).toList();
+      return filterPeople;
+    }catch(error){
+      throw Exception( error);
+    }
+  }
 }
