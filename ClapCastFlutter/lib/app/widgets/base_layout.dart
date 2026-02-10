df97d2633ee828cb20d867076e6c95f8960223d2
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-10 23:45:07 +0100

    Create base_layout widget to simplify screen creation

---
+++ ClapCastFlutter/lib/app/widgets/base_layout.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'navigation_bar.dart';
+
+class BaseLayout extends StatelessWidget {
+  final Widget child;
+  final int currentIndex;
+
+  const BaseLayout({
+    super.key,
+    required this.child,
+    required this.currentIndex
+  });
+
+  void _onItemTapped(BuildContext context, int index) {
+    if (index == currentIndex) return;
+
+    switch (index) {
+      case 0:
+        Navigator.pushReplacementNamed(context, '/dashboard_screen');
+        break;
+      case 1:
+        Navigator.pushReplacementNamed(context, '/search_screen');
+        break;
+      default:
+        Navigator.pushReplacementNamed(context, '/dashboard_screen');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: NavigationBarFooter(
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index) => _onItemTapped(context, index),
+      ),
+    );
+  }
+}
