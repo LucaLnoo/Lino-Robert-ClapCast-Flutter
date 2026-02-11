fd52556ed84dc80bab4d12487e1a00afb91da558
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-12 00:59:48 +0100

    Customize Qr code style

--- ClapCastFlutter/lib/app/widgets/qr_code.dart
+++ ClapCastFlutter/lib/app/widgets/qr_code.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
+import '../../generated/assets.dart';
+import '../../ressources/app_color.dart';
+
 class QrCodeClapCast extends StatelessWidget {
   final String url;
   const QrCodeClapCast({super.key, required this.url});
 
   @override
   Widget build(BuildContext context) {
+
     return QrImageView(
       data: url,
       version: QrVersions.auto,
       size: 200.0,
-      backgroundColor: Colors.white,
+
+      eyeStyle: const QrEyeStyle(
+        eyeShape: QrEyeShape.square,
+        color: AppColor.appContrast,
+      ),
+
+      dataModuleStyle: const QrDataModuleStyle(
+        dataModuleShape: QrDataModuleShape.circle,
+        color: AppColor.appContrast,
+        ),
     );
   }
 }
