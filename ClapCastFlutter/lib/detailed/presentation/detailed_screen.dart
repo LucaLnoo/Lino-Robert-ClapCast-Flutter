f4f0450ad1a37a8615715715eb16378c05eb7f0d
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-12 00:11:02 +0100

    detailed_screen add functions to build actor or movie content (with image description and row with their movies or casts)

--- ClapCastFlutter/lib/detailed/presentation/detailed_screen.dart
+++ ClapCastFlutter/lib/detailed/presentation/detailed_screen.dart
@@ -1,6 +1,8 @@
 import 'package:clapcastflutter/app/model/movie.dart';
 import 'package:clapcastflutter/app/model/person.dart';
+import 'package:clapcastflutter/app/widgets/ContentScrollRow.dart';
 import 'package:clapcastflutter/app/widgets/header.dart';
+import 'package:clapcastflutter/app/widgets/imageApi.dart';
 import 'package:clapcastflutter/ressources/app_color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +10,7 @@ import 'package:provider/provider.dart';
 
 import '../../app/widgets/background.dart';
 import '../../app/widgets/base_layout.dart';
+import '../../search/presentation/category_screen.dart';
 import '../domain/detailed_notifier.dart';
 
 class DetailedScreen extends StatefulWidget {
@@ -52,30 +55,150 @@ class _DetailedScreenState extends State<DetailedScreen> {
 
   Widget _buildBody(DetailedNotifier notifier) {
     if (widget.isAPerson && notifier.person == null) {
-      return const Center(child: CircularProgressIndicator(color: AppColor.white));
+      return const Center(
+          child: CircularProgressIndicator(color: AppColor.white));
     }
     if (!widget.isAPerson && notifier.movie == null) {
-      return const Center(child: CircularProgressIndicator(color: AppColor.white));
+      return const Center(
+          child: CircularProgressIndicator(color: AppColor.white));
     }
 
     return SafeArea(
-      child:
-      Column(
+      child: Column(
         children: [
-          if(widget.isAPerson)
+          if (widget.isAPerson) ...[
             _buildActorContent(notifier.person!)
-          else
-            _buildMovieContent(notifier.movie!)
+          ] else
+            ...[
+              _buildMovieContent(notifier.movie!)
+            ]
         ],
-      )
+      ),
     );
   }
 
   Widget _buildActorContent(PersonDetails person) {
-    return SectionBar(sectionName: person.name ?? "Unknown");
+    final List<MovieOverview>? moviesList = person.knownForMovies?.cast.map((movie) {
+      return MovieOverview(
+        id: movie.id,
+        title: movie.title,
+        posterPathImage: movie.posterPathImage,
+        popularityRate: movie.popularityRate,
+        releaseDate: movie.releaseDate,
+        score: movie.score,
+      );
+    }).toList();
+
+    return Column(
+      children: [
+        SectionBar(
+            sectionName: person.name ?? "Unknown",
+            onBackClick: () => Navigator.pop(context)
+        ),
+        _buildImage(person.profilePathImage),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            person.biography ?? "No biography",
+            style: const TextStyle(
+              color: AppColor.white,
+              fontSize: 14.0,
+              height: 1.5,
+            ),
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        ContentScrollRowMovie(rowTitle: "Known for:",
+          movies: moviesList,
+          onMovieClick: (movie) => _navigateToDetail(movie.id, false),
+          onMoreClick: (movies) => _navigateToCategory("Distribution", people: [], movies: movies),
+        )
+      ],
+    );
   }
 
   Widget _buildMovieContent(MovieDetails movie) {
-    return SectionBar(sectionName: movie.title ?? "Unknown");
+    final List<PersonOverview>? castList = movie.distribution?.cast.map((castMember) {
+      return PersonOverview(
+        id: castMember.personId,
+        name: castMember.actorName,
+        profilePathImage: castMember.profilePathImage,
+        popularityRate: castMember.popularityRate,
+      );
+    }).toList();
+
+    return Column(
+      children: [
+        SectionBar(
+            sectionName: movie.title ?? "Unknown",
+            onBackClick: () => Navigator.pop(context)
+        ),
+        _buildImage(movie.posterPathImage),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            movie.overview ?? "No description",
+            style: const TextStyle(
+              color: AppColor.white,
+              fontSize: 14.0,
+              height: 1.5,
+            ),
+            maxLines: 4,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        ContentScrollRowCast(rowTitle: "Distribution",
+            castMembers: castList,
+            onPersonClick: (person) => _navigateToDetail(person.id, true),
+            onMoreClick: (people) => _navigateToCategory("Distribution", people: people, movies: []),
+        )
+      ],
+    );
+  }
+
+  Widget _buildImage(String? path) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+
+      child: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          child: AspectRatio(
+            aspectRatio: 2 / 3,
+            child: ImageApi(
+              imagePath: path ?? "",
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _navigateToDetail(int id, bool isPerson) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailedScreen(
+          contentId: id,
+          isAPerson: isPerson,
+        ),
+      ),
+    );
+  }
+
+  void _navigateToCategory(String title, {required List<PersonOverview> people, required List<MovieOverview> movies}) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CategoryScreen(
+          categoryTitle: title,
+          people: people,
+          movies: movies,
+        ),
+      ),
+    );
   }
 }
