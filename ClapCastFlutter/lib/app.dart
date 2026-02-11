641d420deb06b8850bbdb603fc94fa04dada103a
p2309118 <luca.lino@etu.univ-lyon1.fr>
2026-02-11 19:45:37 +0100

    Add category screen to navigation

--- ClapCastFlutter/lib/app.dart
+++ ClapCastFlutter/lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:clapcastflutter/ressources/app_color.dart';
 import 'package:clapcastflutter/ressources/app_theme.dart';
 import 'package:clapcastflutter/search/domain/search_notifier.dart';
+import 'package:clapcastflutter/search/presentation/category_screen.dart';
 import 'package:clapcastflutter/search/presentation/search_screen.dart';
 import 'package:clapcastflutter/starting/domain/starting_notifier.dart';
 import 'package:clapcastflutter/starting/presentation/splash_screen.dart';
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
           "/": (context) => const SplashScreen(),
           "/dashboard_screen": (context) => const DashboardScreen(),
           "/search_screen": (context) => const SearchScreen(),
+          "/category_screen": (context) => const CategoryScreen(people: [], movies: [], categoryTitle: '',),
         },
         title: 'Flutter Demo',
         theme: AppTheme.themeData,
